874433a461932dbd1ac74915a37935e8f73ecc94
Owen Throup <38217027+Throupy@users.noreply.github.com>
2018-12-27 19:43:30 +0000

    Better code

--- Code/finalscript.py
+++ Code/finalscript.py
@@ -3,50 +3,54 @@ import xbox
 import time 
 from gpiozero import DistanceSensor, Buzzer, CamJamKitRobot
 
+class Robot:
+	def __init__(self, joystick):
+		self.robot = CamJamKitRobot()  #Initialise Robot
+		self.joy = joystick
+
+	def listen(self):
+		"""Method to start listening for controller presses"""
+		try:
+			while True:
+				self.robot.stop() #Initialise Robot by stopping.
+				if self.joy.rightTrigger() != 0: #On right trigger press
+					self.moveForwards(joy.rightTrigger())
+				if self.joy.leftTrigger() != 0: #On left trigger press
+					self.moveBackwards(joy.leftTrigger())
+				if self.joy.leftStick()[0] != 0: #On left stick move
+					self.turn(self.joy.leftStick()[0])
+				time.sleep(0.2) #Wait between iterations
+		except KeyboardInterrupt: #CTRL-C
+			print("Exiting")
+		finally:
+			self.robot.stop() #Stop robot
+			self.joy.close() #Cleanup
+
+
+	def moveBackwards(self, value):
+		"""Method to move the robot backwards"""
+		if value < 0.1: #If the value is less than 0.1 do nothing, because the wheels don't even have enough power to move.
+			return
+		self.robot.backward(value) #If value is more than 0.1, move with the desired power
+
+	def moveForwards(self, value):
+		"""Method to move the robot forwards"""
+		if value < 0.1:
+			return
+		self.robot.forward(value)
+
+	def turn(self, value):
+		"""Method to turn the robot"""
+		if value < 0:
+			value = value * -1
+			if value < 0.1:
+				return
+			self.robot.left(value)
+		else:
+			if value < 0.1:
+				return
+			self.robot.right(value)
+
 joy = xbox.Joystick() #Initialise joystick (xbox 360 controller)
-robot = CamJamKitRobot() #Initialise Robot
-
-def moveBackwards(value):
-    if value < 0.1: #If the value is less than 0.1
-	   return #Do nothing, because at 0.1 the wheels don't even have enough power to turn - no point doing anything.
-    robot.backward(value) #If the value is more than 0.1, move the robot back with the desired power
-
-def moveForwards(value):
-    if value < 0.1: #If the value is less than 0.1
-       return #Do nothing, because at 0.1 the wheels don't even have enough power to turn - no point doing anything.
-    robot.forward(value) #If the value is more than 0.1, move the robot forward with the desired power
-
-def turn(value):
-    if value < 0: #If the value is negative (or the stick is to the left)
-        value = value * -1 #Make the value positive
-    	if value < 0.1: #If the value is less than 0.1
-	       return #Do nothing, because at 0.1 the wheels don't even have enough power to turn - no point doing anything.
-	robot.left(value) #If the value is more than 0.1, turn the robot left with the desired power
-    else: #If the value is NOT negative (or the stick is to the right)
-	   if value < 0.1: #If the value is less than 0.1
-	       return #Do nothing, because at 0.1 the wheels don't even have enough power to turn - no point doing anything.
-	   robot.right(value) #If the value is more than 0.1, turn the robot right with the desired power
-
-
-
-try:
-    while True: #Loop
-        robot.stop() #Stop the robot (initialise)
-        if joy.rightTrigger() != 0: #If a value from the right trigger is retreived 
-		    moveForwards(joy.rightTrigger()) #Move forwards with the value retreived
-        if joy.leftTrigger() != 0: #If a value from the left trigger is retreived 
-		    moveBackwards(joy.leftTrigger()) #Move backwards with the value retreived
-        if joy.leftStick()[0] != 0: #[0] = x value (tuple is returned - (x,y))
- 		    turn(joy.leftStick()[0]) #Turn with value
-        
-        time.sleep(0.2) #Wait a little before re-iteration
-
-except KeyboardInterrupt: # CTRL-C Exit
-    print("Exiting") #Exit message
-
-
-
-
-
-robot.stop()
-
+robot = Robot(joy) #Instantiate robot class with joystick
+robot.listen() #Start listening
