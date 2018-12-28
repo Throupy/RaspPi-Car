5daa7f3a7f04ef660be1064f3bbca2a8d3475dc2
Owen Throup <38217027+Throupy@users.noreply.github.com>
2018-12-28 15:12:53 +0000

    Added website and camera functionality

--- Site/app.py
+++ Site/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template  
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, redirect
+from gpiozero import CamJamKitRobot
+import time
 
+app = Flask(__name__)
+robot = CamJamKitRobot()
 @app.route("/")
 def home():
 	return render_template("home.html")
+
+robot.stop()
 
 if __name__ == '__main__':
 	app.run(debug=True)
